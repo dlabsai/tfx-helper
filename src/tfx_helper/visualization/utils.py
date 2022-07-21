40062f1a17b8c36c9a5aae1e33fab30459399ced
Tomasz Maćkowiak <kurazu@kurazu.net>
2022-07-21 14:08:20 +0200

    [NOJIRA] fix proto loading util to support GCS paths

--- src/tfx_helper/visualization/utils.py
+++ src/tfx_helper/visualization/utils.py
@@ -1,15 +1,14 @@
 from typing import Type, TypeVar
 
+import tensorflow as tf
 from google.protobuf.message import Message
 
 ProtoClass = TypeVar("ProtoClass", bound=Message)
 
 
-def read_binary_proto(
-    file_path: str, proto_class: Type[ProtoClass]
-) -> ProtoClass:
+def read_binary_proto(file_path: str, proto_class: Type[ProtoClass]) -> ProtoClass:
     instance: ProtoClass = proto_class()
-    with open(file_path, "rb") as f:
+    with tf.io.gfile.GFile(file_path, "rb") as f:
         data = f.read()
     instance.ParseFromString(data)
     return instance
